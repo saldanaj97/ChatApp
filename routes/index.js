856b227413cb6a9a4b8a438530fcdeade6e99a7a
Juan Saldana <saldanaj97@gmail.com>
2022-05-18 15:26:44 -0500

    Removed unused lines of code

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.send('Received a GET request. ');
+});
+
+router.post('/', (req, res) => {
+  res.send('Received a POST request. ');
+});
+
+router.put('/', (req, res) => {
+  res.send('Received a PUT request. ');
+});
+
+router.delete('/', (req, res) => {
+  res.send('Received a DELETE request.');
 });
 
 module.exports = router;
