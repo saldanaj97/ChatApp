7b234e7c3aca3e51c5b26eb4a8b9bf951f8e8a2f
Juan Saldana <saldanaj97@gmail.com>
2022-05-19 15:48:38 -0500

    Button now works for sending req to backend.

---
+++ client/src/Messages.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Messages extends Component {
+    constructor(props) {
+        super(props)
+        this.resFromServer = ''
+        this.getResponse = this.getResponse.bind(this)
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3000/')
+            .then(response => response.text())
+            .then(res => {
+                this.resFromServer = res
+            })
+            .catch(error => console.log(error))
+    }
+
+    getResponse() {
+        console.log(this.resFromServer)
+        return this.resFromServer
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Click the button for a response</h1>
+                <button onClick={this.getResponse}>Get response</button>
+            </div>
+        )
+    }
+
+}
+
+export default Messages;
