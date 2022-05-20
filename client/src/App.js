a2187d6d5e8779f80721b91d5472a59d3e80f536
Juan Saldana <saldanaj97@gmail.com>
2022-05-20 15:21:05 -0500

    Removed unused functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Messages from './Messages'
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      resFromServer: null
-    }
-  }
-
-  componentDidMount() {
-    fetch('http://localhost:3000/')
-      .then(response => response.text())
-      .then(res => console.log(res))
-      .catch(error => console.log(error))
-  }
-
   render() {
-    return (this.resFromServer)
+    return (
+      <div className="App">
+        <Messages />
+      </div>
+    );
   }
-
 }
 
 export default App;
