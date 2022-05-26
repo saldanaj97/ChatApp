47086b3697db743e30ce3b805e9317b20e434717
Juan Saldana <saldanaj97@gmail.com>
2022-05-26 14:43:13 -0500

    Added components to render the messages

---
+++ chat-client/src/Components/MessageInput.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './MessageInput.css';
+
+// Handle the input from the message input box
+const NewMessage = ({ socket }) => {
+    const [value, setValue] = useState('');
+    const submitForm = (e) => {
+        e.preventDefault()
+        socket.emit('message', value);
+        setValue('');
+    };
+
+    return (
+        <form onSubmit={submitForm}>
+            <input autoFocus value={value} placeholder='Type your message' onChange={(e) => { setValue(e.currentTarget.value) }} />
+        </form>
+    );
+};
+
+export default NewMessage;
