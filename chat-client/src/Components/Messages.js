463c6c09eaa88f917fbb47f664195f178f82250a
Juan Saldana <saldanaj97@gmail.com>
2022-05-27 14:00:52 -0500

    Added a message to show when there are no messages

--- chat-client/src/Components/Messages.js
+++ chat-client/src/Components/Messages.js
@@ -33,16 +33,22 @@ function Messages({ socket }) {
 
     return (
         <div className='message-list'>
-            {[...Object.values(messages)]
-                .sort((a, b) => a.time - b.time)
-                .map((message) => (
-                    <div key={message.id} className='message-container' title={`Sent at ${new Date(message.time).toLocaleTimeString()}`}>
-                        <span className="user">{message.user.name}:</span>
-                        <span className="message">{message.value}</span>
-                        <span className="date">{new Date(message.time).toLocaleTimeString()}</span>
-                    </div>
-                ))
+            {[...Object.values(messages)].length === 0 &&
+                <div>
+                    <h2>There are currently no messages in the chat. </h2>
+                </div>
+            } {
+                [...Object.values(messages)]
+                    .sort((a, b) => a.time - b.time)
+                    .map((message) => (
+                        <div key={message.id} className='message-container' title={`Sent at ${new Date(message.time).toLocaleTimeString()}`}>
+                            <span className="user">{message.user.name}:</span>
+                            <span className="message">{message.value}</span>
+                            <span className="date">{new Date(message.time).toLocaleTimeString()}</span>
+                        </div>
+                    ))
             }
+
         </div>
     );
 }
