6cd1a237be62db220876ff6ad90a60536b728715
Juan Saldana <saldanaj97@gmail.com>
2022-05-31 14:41:15 -0500

    Added a basic heading msg to test routing.

--- chat-client/src/Components/Rooms/Chat1.js
+++ chat-client/src/Components/Rooms/Chat1.js
@@ -1,1 +1,9 @@
 import React from 'react'
+
+const ChatRoomOne = () => {
+    return (
+        <div>
+            <h1>Chat room 1</h1>
+        </div>
+    )
+}
--- chat-client/src/Components/Rooms/Chat3.js
+++ chat-client/src/Components/Rooms/Chat3.js
@@ -1,1 +1,9 @@
 import React from 'react'
+
+const ChatRoomOne = () => {
+    return (
+        <div>
+            <h1>Chat room 3</h1>
+        </div>
+    )
+}
