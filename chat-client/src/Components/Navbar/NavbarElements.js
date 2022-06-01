bf4d4162bfdd8ba000a4b063e53b1eb965d4f8c0
Juan Saldana <saldanaj97@gmail.com>
2022-05-31 19:23:24 -0500

    Basic navbar styling

--- chat-client/src/Components/Navbar/NavbarElements.js
+++ chat-client/src/Components/Navbar/NavbarElements.js
@@ -1,71 +1,77 @@
 import { NavLink as Link } from "react-router-dom";
 import styled from 'styled-components';
 
+/* The main colors that will be used */
+let mainColor = '#FA2849'
+let secondaryColor = '#110F15'
+let tertiaryColor = '#423399'
+
+/* Navbar Div */
 export const Nav = styled.nav`
-  background: #63D471;
-  height: 85px;
+  background: #110F15;
+  height: 45px;
   display: flex;
-  justify-content: space-between;
   padding: 0.2rem calc((100vw - 1000px) / 2);
   z-index: 12;
-  /* Third Nav */
-  /* justify-content: flex-start; */
 `;
 
+/* Styling for the menu items that ARE NOT BUTTONS */
 export const NavLink = styled(Link)`
-  color: #808080;
+  color: #FA2849;
   display: flex;
   align-items: center;
+  font-weight: 400;
+  font-size: 21px;
   text-decoration: none;
   padding: 0 1rem;
   height: 100%;
   cursor: pointer;
-  &.active {
-    color: #000000;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    color: #FFFFFF;
   }
 `;
 
+/* All the menu items in the navbar that are not the buttons on the right*/
 export const NavMenu = styled.div`
   display: flex;
   align-items: center;
-  margin-right: -24px;
-  /* Second Nav */
-  /* margin-right: 24px; */
-  /* Third Nav */
-  /* width: 100vw;
-  white-space: nowrap; */
-  @media screen and (max-width: 768px) {
+  margin-right: 24px;
+  @media screen and (max-width: 200px) {
     display: none;
   }
 `;
 
-export const NavBtn = styled.nav`
+/* Group of buttons that will be on the right side */
+export const NavBtnGroup = styled.nav`
   display: flex;
   align-items: center;
   margin-right: 24px;
-  /* Third Nav */
-  /* justify-content: flex-end;
-  width: 100vw; */
-  @media screen and (max-width: 768px) {
+  justify-content: flex-end;
+  width: 100vw;
+  @media screen and (max-width: 200px) {
     display: none;
   }
 `;
 
+/* Styling for the nav button links */
 export const NavBtnLink = styled(Link)`
   border-radius: 4px;
-  background: #808080;
-  padding: 10px 22px;
-  color: #000000;
-  outline: none;
-  border: none;
+  padding: 10px 10px;
+  color: #FA2849;
   cursor: pointer;
-  transition: all 0.2s ease-in-out;
   text-decoration: none;
-  /* Second Nav */
-  margin-left: 24px;
+  font-weight: 400;
+  font-size: 18px;
+  margin-left: 10px;
+  transition: all 0.2s ease-in-out;
   &:hover {
     transition: all 0.2s ease-in-out;
-    background: #fff;
-    color: #808080;
+    background: #FA2849;
+    color: #FFFFFF;
+  }
+  &.active {
+    background: #FA2849;
+    color: #FFFFFF;
   }
 `;
