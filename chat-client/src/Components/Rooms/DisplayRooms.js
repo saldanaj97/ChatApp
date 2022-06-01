6c2cdd4c7b6854534036213b06b1d8e12abf6fef
Juan Saldana <saldanaj97@gmail.com>
2022-05-31 19:25:52 -0500

    Moved the messaging code to new files.

--- chat-client/src/Components/Rooms/DisplayRooms.js
+++ chat-client/src/Components/Rooms/DisplayRooms.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import './chat.css';
 
 const DisplayRooms = () => {
     return (
-        <div>
+        <div className='main-div'>
             <h1>All Chatrooms</h1>
         </div>
     )
