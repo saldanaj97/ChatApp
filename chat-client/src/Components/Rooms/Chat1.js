cfc7dcf6853ba59546c93ead91788f0ba4efb013
Juan Saldana <saldanaj97@gmail.com>
2022-06-01 18:10:10 -0500

    Chat room one now working on port 3000.

--- chat-client/src/Components/Rooms/Chat1.js
+++ chat-client/src/Components/Rooms/Chat1.js
@@ -1,26 +1,23 @@
-import React, { useEffect } from 'react'
-/* import io from 'socket.io-client';
-import Messages from './Components/Messaging/Messages';
-import MessageInput from './Components/Messaging/MessageInput'; */
+import React, { useState, useEffect } from 'react'
+import io from 'socket.io-client';
+import Messages from '../Messaging/Messages';
+import MessageInput from '../Messaging/MessageInput';
+import './ChatStyles';
+import { ChatroomPageContainer, PageHeading } from './ChatStyles';
 
 const ChatRoomOne = () => {
-  return (
-    <div>
-      <h1>Chat room 1</h1>
-    </div>
-  )
-}
-
-// Establish the connection
-/*     
-    const [socket, setSocket] = useState(null);
-    useEffect(() => {
+  // Establish the connection
+  const [socket, setSocket] = useState(null);
+  useEffect(() => {
     const newSocket = io(`http://${window.location.hostname}:3000`);
     setSocket(newSocket);
     return () => newSocket.close();
-  }, [setSocket]);  // If the connection has been established, display the messages 
+  }, [setSocket]);
+
+  // If the connection has been established, display the messages 
   return (
-    <div className='App'>
+    <ChatroomPageContainer>
+      <PageHeading>Room One</PageHeading>
       {socket ? (
         <div className='chat-container'>
           <Messages socket={socket} />
@@ -29,7 +26,8 @@ const ChatRoomOne = () => {
       ) : (
         <div>Not Connected To Server</div>
       )}
-    </div>
-  ) */
+    </ChatroomPageContainer>
+  )
+}
 
 export default ChatRoomOne;
