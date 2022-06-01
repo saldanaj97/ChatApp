97f01261fd185b29f7e29682dc4402a58cca3b0c
Juan Saldana <saldanaj97@gmail.com>
2022-06-01 16:58:58 -0500

    Added the new rotues to each chat room

--- chat-client/src/App.js
+++ chat-client/src/App.js
@@ -4,6 +4,9 @@ import DisplayRooms from './Components/Rooms/DisplayRooms';
 import Navbar from './Components/Navbar/Navbar';
 import Login from './Components/User/Login';
 import Signup from './Components/User/Signup';
+import ChatRoomOne from './Components/Rooms/Chat1';
+import ChatRoomTwo from './Components/Rooms/Chat2';
+import ChatRoomThree from './Components/Rooms/Chat3';
 import './App.css';
 
 function App() {
@@ -14,6 +17,9 @@ function App() {
         <Route path='/' exact element={<DisplayRooms />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/roomone' element={<ChatRoomOne />} />
+        <Route path='/roomtwo' element={<ChatRoomTwo />} />
+        <Route path='/roomthree' element={<ChatRoomThree />} />
       </Routes>
     </Router>
   );
