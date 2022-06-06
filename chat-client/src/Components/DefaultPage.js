e0983140038727c3ff6d7d4830ebc2a899a615fe
Juan Saldana <saldanaj97@gmail.com>
2022-06-06 15:20:55 -0500

    Updated deprecated import

--- chat-client/src/Components/DefaultPage.js
+++ chat-client/src/Components/DefaultPage.js
@@ -4,8 +4,8 @@ import { BiArrowBack } from 'react-icons/bi'
 import { useNavigate } from 'react-router-dom'
 
 export default function DefaultPage() {
-    const history = useNavigate()
-    const redirect = () => history.push('/')
+    const navigate = useNavigate()
+    const redirect = () => navigate('/')
     return (
         <div>
             <IconButton mr={2} isRound='true' bg='green.300' color='white' icon={<BiArrowBack />} onClick={redirect} /> You're lost. Go home.
