38ee14bf555ae374b92a3b7e66b578d381cf94dc
Juan Saldana <saldanaj97@gmail.com>
2022-06-14 14:37:17 -0500

    Basic bio box

--- chat-client/src/Components/Login/Bio.js
+++ chat-client/src/Components/Login/Bio.js
@@ -3,7 +3,11 @@ import { Avatar, Box, Flex, Heading, IconButton, Text, Menu, Button, MenuButton,
 
 const Bio = () => {
     return (
-        <Box background='white' zIndex='overlay'>Hey</Box>
+        <Flex direction='column' justifyContent='center' bg=''>
+            <Flex justifyContent='center'><Avatar className='avatar' size='md' /></Flex>
+            <Text textAlign='center' color='white' fontSize='16px'>Test Name</Text>
+            <Flex m='5px 5px'><Text color='white' fontSize='13px'>This will be a test bio that will hold whatever the user wants to say. </Text></Flex>
+        </Flex >
     )
 }
 
