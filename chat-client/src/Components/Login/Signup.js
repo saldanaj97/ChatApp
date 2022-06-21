0bafce9f7a2d23597011ac7b0b73ce64036ce039
Juan Saldana <saldanaj97@gmail.com>
2022-06-21 13:02:43 -0500

    Styling updates for signup page

--- chat-client/src/Components/Login/Signup.js
+++ chat-client/src/Components/Login/Signup.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 
-import { Box, Button, Divider, Flex, FormControl, Input, Text } from "@chakra-ui/react"
+import { Box, Button, Center, Divider, Flex, FormControl, Input, Text } from "@chakra-ui/react"
 
 import { SignupContext } from './SignupContext'
 
@@ -14,43 +14,44 @@ const Signup = () => {
     }
 
     return (
-        <Flex className='signup-container' direction='row' width={{ base: '100%', sm: '950px' }} height={{ base: '100%', sm: '450px' }}>
-
-            {/* Small bio about what the user is registering for */}
-            <Flex className='registration-info'>
-                <Flex className='title-container' margin='auto auto'>
-                    <Text className='reg-info-title'>Create a chatroom.io account</Text>
-                </Flex>
-                <Flex className='description-container' width='80%' margin='auto auto'>
+        <Flex className='signup-container' direction='column' width={{ base: '100%', sm: '450px' }} height={{ base: '100%', sm: '500px' }} >
+            <Flex className='reg-title-and-desc' direction='column' m='15px'>
+                <Box className='title-container' textAlign='center'>
+                    <Text className='reg-info-title'>Create an account</Text>
+                </Box>
+                <Center className='description-container' m='5px 20px' textAlign='center'>
                     <Text className='reg-info-description'>
-                        An account is needed to add friends, be included in groups, and participate in chatrooms.
-                        Don’t worry, your email will not be shared with anyone, it will only be used for account creation.
+                        An account is required to add friends, be included in groups, and participate in chatrooms.
                     </Text>
-                </Flex>
-                <Flex className='title-container'>
-                    <Text className='existing-user-title'>Already have an account?</Text>\
-                </Flex>
-                <Flex className='existing-user-button'>
-                    <Button backgroundColor='#FA2849' isRound='true' width='25%' m='0px 10px' onClick={handleLoginClick}>Login</Button>
-                </Flex>
+                </Center>
             </Flex>
 
-            {/* Center Divider */}
-            <Flex height='90%' margin='auto'><Divider orientation='vertical' borderColor='#FA2849' /></Flex>
-
             {/* Sign up forms */}
-            <Flex className='signup-forms' justify='center'>
-                <Text className='signup-title' width='50%' margin='0 auto' >Sign up</Text>
-                <Box width='50%' margin='0px auto'>
+            <Flex className='signup-forms' m='15px auto'>
+                <Text className='signup-title' textAlign='left'>Sign up</Text>
+                <Box width='100%' margin='0px auto'>
                     <FormControl>
                         <Input variant='flushed' id='firstname' focusBorderColor=' #FA2849' placeholder='Username' />
                         <Input variant='flushed' id='password' focusBorderColor=' #FA2849' type='password' placeholder='Password' />
                         <Input variant='flushed' id='confirm-pass' focusBorderColor=' #FA2849' type='password' placeholder='Confirm Password' />
                     </FormControl>
                 </Box>
-                <Flex width='50%' justifyContent='flex-end' m='5px auto'><Button bg='#FA2849'>Submit</Button></Flex>
+                <Flex width='100%' justifyContent='center' m='5px auto'><Button bg='#FA2849'>Sign up</Button></Flex>
             </Flex>
-        </Flex>
+
+            {/* Divider */}
+            <Flex margin='auto' width='100%'><Divider borderColor='#FA2849' orientation='horizontal' /></Flex>
+
+            {/* Login option for existing users */}
+            <Flex className='existing-user' direction='column' m='10px'>
+                <Box className='title-container' textAlign='center'>
+                    <Text className='existing-user-title'>Already have an account?</Text>
+                </Box>
+                <Flex className='existing-user-button' justify='center' m='10px 0 10px 0'>
+                    <Button backgroundColor='#FA2849' isRound='true' width='25%' onClick={handleLoginClick}>Login</Button>
+                </Flex>
+            </Flex >
+        </Flex >
     )
 }
 
