710b8908cc76fdd930300f150750de5ffe238fd8
Juan Saldana <saldanaj97@gmail.com>
2022-06-21 13:13:34 -0500

    Removed drop shadow from containers

--- chat-client/src/Components/Login/Home.js
+++ chat-client/src/Components/Login/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
   const { showSignUp, setShowSignUp } = useContext(SignupContext)
 
   return (
-    <Flex direction='column'>
+    <Flex direction='column' filter='dropShadow(16px 16px 10px black)'>
       {/* Title */}
       <Heading as="h1" size="3xl" textAlign='center' mb='20px' fontFamily='DM Sans' fontWeight='600' letterSpacing='-2px' color='#FA2849'>Chatroom.io</Heading>
       <Collapse startingHeight={450} in={showSignUp}>
