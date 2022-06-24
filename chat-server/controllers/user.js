b524e8d4cf2993ff00120cbc2c069167f6a70f68
Juan Saldana <saldanaj97@gmail.com>
2022-06-24 15:20:50 -0500

    Create user API setup

--- chat-server/controllers/user.js
+++ chat-server/controllers/user.js
@@ -1,6 +1,42 @@
-export default {
-    onGetAllUsers: async (req, res) => { },
-    onGetUserById: async (req, res) => { },
-    onCreateUser: async (req, res) => { },
-    onDeleteUserById: async (req, res) => { },
+/* Models */
+import UserModel, { USER_TYPES } from '../models/User.js'
+import validateUserReqBody from './helpers/validate/index.js'
+
+/* Helper function that will check to see if the data is as follows:
+    - firstName, lastName, email: String
+    - type: Array
+*/
+
+const onGetAllUsers = async (req, res) => { }
+
+const onGetUserById = async (req, res) => { }
+
+const onCreateUser = async (req, res) => {
+  try {
+    /* Make sure the data is valid before sending the post req, if not notify the user with errors pointing to
+     what is wrong */
+    const validated = validateUserReqBody(types => ({
+      payload: req.body,
+      checks: {
+        firstName: { type: types.string },
+        lastName: { type: types.string },
+        type: { type: types.enum, options: { enum: USER_TYPES } }
+      }
+    }))
+    if (!validated.success) return res.status(400).json({ validated })
+
+    /* Now that the data has been validated above, we can send the req and res to user to notify them
+     of a succssful account creation */
+    const { firstName, lastName, type } = req.body
+    const user = await UserModel.createUser(firstName, lastName, type)
+    return res.status(200).json({ sucess: true, user })
+  } catch (error) {
+    /* Return the error that has been caught */
+    console.log(error)
+    return res.status(500).json({ success: false, error })
+  }
 }
+
+const onDeleteUserById = async (req, res) => { }
+
+export default { onGetAllUsers, onGetUserById, onCreateUser, onDeleteUserById }
--- chat-server/models/User.js
+++ chat-server/models/User.js
@@ -2,24 +2,37 @@ import mongoose from "mongoose"
 import { v4 as uuidv4 } from 'uuid'
 
 export const USER_TYPES = {
-    CONSUMER: 'consumer',
-    SUPPORT: 'support',
+  CONSUMER: 'consumer',
+  SUPPORT: 'support',
 }
 
+/* Table schema for a user. 
+    - id: randomly get a string from uuidv4
+    - type: either consumer or support 
+*/
 const userSchema = new mongoose.Schema(
-    {
-        _id: {
-            type: String,
-            deafult: () => uuidv4().replace(/\-/g, ""),
-        },
-        firstName: String,
-        lastName: String,
-        type: String,
+  {
+    _id: {
+      type: String,
+      default: () => uuidv4().replace(/\-/g, ""),
     },
-    {
-        timestamps: true,
-        collection: 'users',
-    }
+    firstName: String,
+    lastName: String,
+    type: String,
+  },
+  {
+    timestamps: true,
+    collection: 'users',
+  }
 );
+
+userSchema.statics.createUser = async function (firstName, lastName, type) {
+  try {
+    const user = await this.create({ firstName, lastName, type });
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
 
 export default mongoose.model('User', userSchema)
