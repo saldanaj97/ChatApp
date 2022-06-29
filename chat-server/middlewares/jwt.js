f3848599c8cc869ee8f69fa5fb6cec174a4d787d
Juan Saldana <saldanaj97@gmail.com>
2022-06-29 15:25:42 -0500

    Middleware to encode and decode auth token

--- chat-server/middlewares/jwt.js
+++ chat-server/middlewares/jwt.js
@@ -1,5 +1,42 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+import UserModel from "../models/User";
 
-export const decode = (req, res, next) => { }
+const SECRET_KEY = "test key";
 
-export const encode = async (req, res, next) => { }
+/* Function that will check to see if a user is in the DB and if they are, will then assign them a 
+  an authentication token */
+export const encode = async (req, res, next) => {
+  try {
+    const { userID } = req.params;
+    const user = await UserModel.getUserById(userID);
+    const payload = {
+      userID: user._id,
+      userType: user.userType,
+    };
+    const authToken = jwt.sign(payload, SECRET_KEY);
+    console.log("Auth", authToken);
+    req.authToken = authToken;
+    next();
+  } catch (error) {
+    return res.status(400).json({ success: false, message: error.error });
+  }
+};
+
+/* Function that will decode the userId and type from the auth token IF PROVIDED, 
+ otherwise an error will be returned */
+export const decode = (req, res, next) => {
+  if (!req.headers["authorization"]) {
+    return res
+      .status(400)
+      .json({ success: false, error: "No access token provided " });
+  }
+  const accessToken = req.headers["authorization"].split(" ")[1];
+  try {
+    const decoded = jwt.verify(accessToken, SECRET_KEY);
+    req.userId = decoded.userId;
+    req.userType = decoded.userType;
+    return next();
+  } catch (error) {
+    return res.status(401).json({ success: false, error: error.message });
+  }
+};
