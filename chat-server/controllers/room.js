e83ba266cf447fd5d8b1ca60cf53b53efe92e7fe
Juan Saldana <saldanaj97@gmail.com>
2022-07-08 14:29:56 -0500

    New room now created on initiate route.

--- chat-server/controllers/room.js
+++ chat-server/controllers/room.js
@@ -1,7 +1,47 @@
+import validateUserReqBody from "./ValidationHelper/index.js";
+import ChatRoomModel, { CHAT_ROOM_TYPES } from "../models/ChatRoom.js";
+
+/* Function to initaite a chatroom */
+const initiate = async (req, res) => {
+  try {
+    // Validate the req body
+    const validation = validateUserReqBody((types) => ({
+      payload: req.body,
+      checks: {
+        userIds: {
+          type: types.array,
+          options: { unique: true, empty: false, stringOnly: true },
+        },
+        type: { type: types.enum, options: { enum: CHAT_ROOM_TYPES } },
+      },
+    }));
+
+    // If the req is not valid return an error
+    if (!validation.success) return res.status(400).json({ ...validation });
+
+    // If the req has been validated, get the list of users in a room and assign the current authenticated user as the chat initiator
+    const { userIds, type } = req.body;
+    const { userId: chatInitiator } = req;
+
+    // Add the initiator to the group of all users in a room
+    const allUserIds = [...userIds, chatInitiator];
+
+    // Initiate the chatroom
+    const chatRoom = await ChatRoomModel.initiateChat(allUserIds, type, chatInitiator);
+    return res.status(200).json({ success: true, chatRoom });
+  } catch (error) {
+    return res.status(500).json({ success: false, error: error });
+  }
+};
+const postMessage = async (req, res) => {};
+const getRecentConversation = async (req, res) => {};
+const getConversationByRoomId = async (req, res) => {};
+const markConversationReadByRoomId = async (req, res) => {};
+
 export default {
-    initiate: async (req, res) => { },
-    postMessage: async (req, res) => { },
-    getRecentConversation: async (req, res) => { },
-    getConversationByRoomId: async (req, res) => { },
-    markConversationReadByRoomId: async (req, res) => { },
-}
+  initiate,
+  postMessage,
+  getRecentConversation,
+  getConversationByRoomId,
+  markConversationReadByRoomId,
+};
