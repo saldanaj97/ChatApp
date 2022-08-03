1d9b112ad02229cb77177c310dad4e5afb57df98
Juan Saldana <saldanaj97@gmail.com>
2022-08-03 16:10:39 -0500

    Updated to use baseurl

--- chat-client/src/Components/Login/LoginServices.js
+++ chat-client/src/Components/Login/LoginServices.js
@@ -1,15 +1,21 @@
 import axios from "axios";
 
+/* Constants that will be used when making API calls */
+const BASE_URL = "http://localhost:3000";
+const CONFIG = {
+  withCredentials: true,
+};
+
 /* Function to log a user in */
 export const logUserIn = async (name, password, setUserId) => {
-  const { success, userId } = await axios.post(`/login/${name}/${password}`).then((response) => response.data);
+  const { success, userId } = await axios.post(`${BASE_URL}/login/${name}/${password}`, CONFIG).then((response) => response.data);
   setUserId(userId);
   return success;
 };
 
 /* Function to retrieve the most recent conversation */
 export const getRecentConvo = async (setRoomId) => {
-  const { _id } = await axios.get("/room").then((response) => {
+  const { _id } = await axios.get(`${BASE_URL}/room`, CONFIG).then((response) => {
     if (response.data.conversation.length > 0) {
       return response.data.conversation[0];
     }
