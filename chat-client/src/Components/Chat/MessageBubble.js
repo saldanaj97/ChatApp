d640863859d0a7ace658bacc112df696b1bc5011
Juan Saldana <saldanaj97@gmail.com>
2022-08-03 11:53:51 -0500

    Fixed key usage for message bubbles

--- chat-client/src/Components/Chat/MessageBubble.js
+++ chat-client/src/Components/Chat/MessageBubble.js
@@ -6,7 +6,7 @@ export function MessageBubble({ message, i, name }) {
 
   return (
     <Box display='flex' key={i} className={`message ${authorInfo === name ? "my-message" : ""}`} m='.2rem .2rem'>
-      <Box display='flex' key={i} m='.2rem .2rem'>
+      <Box display='flex' m='.2rem .2rem'>
         <Box display='flex' flexDirection='column' className='name-msg-block' justifyContent='center'>
           <Text fontSize='sm' className='msg' p='.4rem .8rem' bg='white' borderRadius='15px' color='black'>
             {messageText}
