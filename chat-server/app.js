df6c5a5548ab9af16119b323fd143b0f8147bd36
Juan Saldana <saldanaj97@gmail.com>
2022-08-05 21:11:37 -0500

    Cors

--- chat-server/app.js
+++ chat-server/app.js
@@ -29,12 +29,18 @@ app.set("port", PORT);
 
 // Cors
 const corsOptions = {
-  origin: ["*"],
+  origin: [""],
   credentials: true,
   exposedHeaders: ["Authorization"],
 };
 
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
