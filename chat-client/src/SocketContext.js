e435fcb3977f988b15d708efa5c58b18b5e50cf3
Juan Saldana <saldanaj97@gmail.com>
2022-08-05 21:34:10 -0500

    Updated endpoint

--- chat-client/src/SocketContext.js
+++ chat-client/src/SocketContext.js
@@ -4,7 +4,7 @@ import io from "socket.io-client";
 const SocketContext = React.createContext();
 
 const SocketProvider = ({ children }) => {
-  const ENDPOINT = "http://localhost:3000/";
+  const ENDPOINT = "https://saldanaj97-chattyio.herokuapp.com/room";
   const socket = io(ENDPOINT, { transports: ["websocket", "polling"] });
 
   return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>;
