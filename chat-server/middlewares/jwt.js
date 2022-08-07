697de5342e4c681e0793d9e090ca39549bdc3cfd
Juan Saldana <saldanaj97@gmail.com>
2022-08-07 15:53:56 -0500

    Went back to old middleware

--- chat-server/middlewares/jwt.js
+++ chat-server/middlewares/jwt.js
@@ -12,7 +12,9 @@ export const encode = async (req, res, next) => {
       return res.status(401).json({ success: false, message: "Invalid login credentials" });
     }
     const token = jwt.sign({ userid: verifiedLogin.user._id, userType: verifiedLogin.user.type }, SECRET_KEY);
-    return res.status(200).json({ success: true, token });
+    res.cookie("Authorization", token, { httpOnly: false });
+    console.log("cookie", res.cookie);
+    return res.status(200).json({ success: true, userId: verifiedLogin.user._id });
   } catch (error) {
     return res.status(400).json({
       success: false,
@@ -24,17 +26,16 @@ export const encode = async (req, res, next) => {
 
 /* Function that will decode the userId and type from the auth token IF PROVIDED, 
  otherwise an error will be returned */
-export const decode = async (req, res, next) => {
+export const decode = (req, res, next) => {
+  if (req.cookies === "") {
+    return res.status(400).json({ success: false, error: "No access token provided " });
+  }
+  const accessToken = req.cookies["Authorization"];
   try {
-    const token = await req.headers.authorization.split(" ")[1];
-    console.log("token", token);
-    const decodedToken = jwt.verify(token, SECRET_KEY);
-    console.log("decodedtoken", decodedToken);
-    const user = decodedToken;
-    console.log("user", user);
-    req.userId = user.userid;
-    req.userType = user.userType;
-    next();
+    const decoded = jwt.verify(accessToken, SECRET_KEY);
+    req.userId = decoded.userid;
+    req.userType = decoded.userType;
+    return next();
   } catch (error) {
     return res.status(401).json({
       success: false,
