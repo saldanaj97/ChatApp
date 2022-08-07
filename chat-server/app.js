0d8c4f0c2f3916c83e19ed4b3e549e2580cec58e
Juan Saldana <saldanaj97@gmail.com>
2022-08-07 15:32:25 -0500

    Edited cors origin

--- chat-server/app.js
+++ chat-server/app.js
@@ -37,7 +37,7 @@ const corsOptions = {
 //app.use(cors(corsOptions));
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
   res.setHeader("Access-Control-Allow-Credentials", true);
