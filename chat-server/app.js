e79568a3343a00f8c056bcbb26a6b637913053b8
Juan Saldana <saldanaj97@gmail.com>
2022-08-07 14:58:58 -0500

    Adjusted local host

--- chat-server/app.js
+++ chat-server/app.js
@@ -35,7 +35,7 @@ const corsOptions = {
 };
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
   next();
