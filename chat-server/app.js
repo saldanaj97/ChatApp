70d44f55679baedd73df0352cab290a8b53c7093
Juan Saldana <saldanaj97@gmail.com>
2022-08-07 15:09:54 -0500

    Adjusted Cors

--- chat-server/app.js
+++ chat-server/app.js
@@ -29,18 +29,20 @@ app.set("port", PORT);
 
 // Cors
 const corsOptions = {
-  origin: ["http://localhost:3000"],
+  origin: ["http://127.0.0.1:3000/"],
   credentials: true,
   exposedHeaders: ["Authorization"],
 };
 
+//app.use(cors(corsOptions));
+app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
