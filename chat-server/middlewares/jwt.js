b33912f038dd4d98b38d890b0245dc5f59e50013
Juan Saldana <saldanaj97@gmail.com>
2022-08-07 17:49:26 -0500

    Redid the encode and decode functions to return jwt instead of assign to cookie.

--- chat-server/middlewares/jwt.js
+++ chat-server/middlewares/jwt.js
@@ -4,17 +4,15 @@ import { SECRET_KEY } from "../app.js";
 
 /* Function that will check to see if a user is in the DB and if they are, will then assign them a 
   an authentication token */
-export const encode = async (req, res, next) => {
+export const encode = async (req, res) => {
   try {
     const { username, password } = req.params;
     const verifiedLogin = await User.onUserLogin(username, password);
     if (verifiedLogin.success === false) {
       return res.status(401).json({ success: false, message: "Invalid login credentials" });
     }
-    const token = jwt.sign({ userid: verifiedLogin.user._id, userType: verifiedLogin.user.type }, SECRET_KEY);
-    res.cookie("Authorization", token, { httpOnly: false, secure: false });
-    console.log("cookie", res.cookie);
-    return res.status(200).json({ success: true, userId: verifiedLogin.user._id });
+    const token = jwt.sign({ userid: verifiedLogin.user._id }, SECRET_KEY);
+    return res.status(200).json({ success: true, message: "Login successful", token });
   } catch (error) {
     return res.status(400).json({
       success: false,
@@ -26,21 +24,18 @@ export const encode = async (req, res, next) => {
 
 /* Function that will decode the userId and type from the auth token IF PROVIDED, 
  otherwise an error will be returned */
-export const decode = (req, res, next) => {
-  if (req.cookies === "") {
-    return res.status(400).json({ success: false, error: "No access token provided " });
-  }
-  const accessToken = req.cookies["Authorization"];
-  console.log("token", accessToken);
+export const decode = async (req, res, next) => {
   try {
-    const decoded = jwt.verify(accessToken, SECRET_KEY);
-    req.userId = decoded.userid;
-    req.userType = decoded.userType;
-    return next();
+    const token = await req.headers.authorization.split("")[1];
+    const decodedToken = jwt.verify(token, SECRET_KEY);
+    const user = decodedToken;
+    req.userId = user;
+    next();
   } catch (error) {
     return res.status(401).json({
       success: false,
       message: "Could not decode authorization token",
+      error: error,
     });
   }
 };
