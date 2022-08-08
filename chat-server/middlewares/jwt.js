786b9fa5abce32e8a912412274ba074da10b2e37
Juan Saldana <saldanaj97@gmail.com>
2022-08-08 12:56:07 -0500

    Added in res.cookie

--- chat-server/middlewares/jwt.js
+++ chat-server/middlewares/jwt.js
@@ -15,6 +15,7 @@ export const encode = async (req, res) => {
       userId: verifiedLogin.user._id,
     };
     const token = jwt.sign(payload, SECRET_KEY);
+    res.cookie("Authorization", token, { httpOnly: true });
     return res.status(200).json({ success: true, userId: verifiedLogin.user._id, token });
   } catch (error) {
     return res.status(400).json({
